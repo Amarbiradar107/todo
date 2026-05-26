f9a27109b3b726a2eb84986115a821e948bd61cc
Amarbiradar107 <amarbiradar107@gmail.com>
2026-05-26 21:29:04 +0530

    added forntend files

--- backend/todo/users/models.py
+++ backend/todo/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Employee(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     department = models.CharField(max_length=50)
