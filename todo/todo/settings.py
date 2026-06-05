3ffd1e456f3759c3579593b2b8083942ed927241
Amarbiradar107 <126169788+Amarbiradar107@users.noreply.github.com>
2026-06-05 09:39:35 +0000

    settinf changes

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o4mik1j1hmtwl^c5#c5^+5zh#7*s0_5k+0@$-^$)jyrs52z73b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.207.113.182','192.168.0.6', '127.0.0.1','justcode.co.in','*']
+ALLOWED_HOSTS = ['3.111.41.8','192.168.0.6', '127.0.0.1','justcode.co.in','*']
 
 
 # Application definition
