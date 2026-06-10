a4f2c65529c80b03dcc310963b7250db6cc7f728
Amarbiradar107 <amarbiradar107@gmail.com>
2026-06-10 22:46:00 +0530

    setting chnages

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql',
-        'USER': 'admin',
-        'PASSWORD': 'Admin1234',
-        'HOST': 'tododb.c3syuegywmhf.ap-south-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mysql',
+#         'USER': 'admin',
+#         'PASSWORD': 'Admin1234',
+#         'HOST': 'tododb.c3syuegywmhf.ap-south-1.rds.amazonaws.com',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
